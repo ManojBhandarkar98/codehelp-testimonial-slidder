6aebbef956e490ccdaa80d656b54ad19b6c904f4
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-11 11:06:14 +0530

    Epi - 06 - Button- Left, Ronght, SurprizeMe-random no generator logic done

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 const Testimonial = (props) => {
     const reviews = props.reviews;
+    const [index, setIndex] = useState(0);
 
     function leftShiftHandler() {
-
+        if (index - 1 < 0) {
+            setIndex(reviews.length - 1);
+        }
+        else {
+            setIndex(index - 1);
+        }
     }
     function rightShiftHandler() {
-
+        if (index + 1 > reviews.length) {
+            setIndex(0);
+        }
+        else {
+            setIndex(index + 1);
+        }
     }
     function surpriseHandler() {
-
+        let randomIndex = Math.floor(Math.random() * reviews.length);
+        setIndex(randomIndex);
     }
     return (
         <div className='w-[85vw] md:w-[700px] bg-white flex flex-col justify-center items-center mt-10 p-10 transition-all duration-700 hover:shadow-xl rounded-xl'>
